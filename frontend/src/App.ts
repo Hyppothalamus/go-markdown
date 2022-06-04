9af767972f469573eb2710d5417ae3dc896d2432
Kasper Haepers <kasper.haepers@outlook.be>
2022-06-04 13:22:21 +0200

    added tailwindcss to react

--- frontend/src/App.ts
+++ frontend/src/App.ts
@@ -1,12 +1,12 @@
 import React from "react";
+import './index.css'
 
-function App(){
-    return React.createElement("div", null, 
-        React.createElement("p", null, 
+function App() {
+    return React.createElement("div", null,
+        React.createElement("h1", {className: "text-3xl font-bold underline"},
             "Hello World"
         ), React.createElement("p", null, "It is I, Kasper")
     )
-
 }
 
 export default App;
