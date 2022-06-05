d69724fd8712484afa4d8d940c0e69cee639daf1
Kasper Haepers <kasper.haepers@outlook.be>
2022-06-05 14:17:32 +0200

    found onChange method :sunglasso:

--- frontend/src/components/LineInput.ts
+++ frontend/src/components/LineInput.ts
@@ -1,7 +1,10 @@
 import React, { FC } from 'react';
 
 const LineInput: FC = () => {
-    return React.createElement('textarea', {className: "block w-full border border-solid border-gray-300", placeholder: "Hello World"}, null)
+    return React.createElement('textarea', {className: "block w-full border border-solid border-gray-300", placeholder: "Hello World", onChange: () => {
+        // TODO: handle keypresses and after enters handle styling and add blocks
+        console.log('hello world!')
+    }}, null)
 }
 
 export default LineInput;
