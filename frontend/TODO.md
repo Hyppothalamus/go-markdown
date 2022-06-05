babc9b7bc9a183536e1ca7c1690323b98e26e370
Kasper Haepers <kasper.haepers@outlook.be>
2022-06-05 15:05:04 +0200

    basic keyhandler and added frontend todo

---
+++ frontend/TODO.md
@@ -0,0 +1,8 @@
+# GO-markdown
+
+## TODO's
+
+* styling
+* add blocks
+* dynamically style the blocks
+    * instead of after enter press
--- frontend/src/components/LineInput.ts
+++ frontend/src/components/LineInput.ts
@@ -1,10 +1,21 @@
 import React, { FC } from 'react';
 
 const LineInput: FC = () => {
-    return React.createElement('textarea', {className: "block w-full border border-solid border-gray-300", placeholder: "Hello World", onChange: () => {
-        // TODO: handle keypresses and after enters handle styling and add blocks
-        console.log('hello world!')
-    }}, null)
+
+    // TODO: move this to context to dynamically add blocks to entire App.ts
+    function handleInput(event: KeyboardEvent) {
+        if (event.keyCode === 13) {
+            console.log(event.key)
+        }
+    }
+
+    return React.createElement('textarea', {
+        className: "block w-full border border-solid border-gray-300",
+        placeholder: "Hello World",
+        onKeyUp: (e: KeyboardEvent) => {
+            handleInput(e)
+        }
+    }, null)
 }
 
 export default LineInput;
