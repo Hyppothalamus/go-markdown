1011d96e651d2bcb13ab8e37e6fc581f11196b5d
Kasper Haepers <kasper.haepers@outlook.be>
2022-06-16 10:21:58 +0200

    some comments because dont feel like coding

--- frontend/src/components/MenuBar.tsx
+++ frontend/src/components/MenuBar.tsx
@@ -1,9 +1,13 @@
 const MenuBar = (): JSX.Element => {
+    // TODO add menu items to this as a bar
+    // TODO make clickable and open menu in same space as item
+    // example: menu item when opens show button to open dir or file
+    // another item opens settings (floating window)
     return (
     <div>
         <p>Hello World</p>
     </div>
-)
+    )
 }
 
 export default MenuBar
