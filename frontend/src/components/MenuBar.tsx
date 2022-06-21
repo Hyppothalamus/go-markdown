5a1e06fadb940b9d55eb87fc6fa103abec1cbecd
Kasper Haepers <kasper.haepers@outlook.be>
2022-06-21 19:56:27 +0200

    some comments and tailwind styling

--- frontend/src/components/MenuBar.tsx
+++ frontend/src/components/MenuBar.tsx
@@ -1,11 +1,13 @@
 const MenuBar = (): JSX.Element => {
     // TODO add menu items to this as a bar
     // TODO make clickable and open menu in same space as item
+    // TODO make baritems a list because these options can change
     // example: menu item when opens show button to open dir or file
     // another item opens settings (floating window)
     return (
-    <div>
-        <p>Hello World</p>
+    <div className= "w-screen flex grid gap-4 grid-cols-2 grid-rows-1">
+        <p>Menu</p>
+        <p>File</p>
     </div>
     )
 }
